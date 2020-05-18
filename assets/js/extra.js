f3da7b5565d80fa2897f75883d26836aa7576387
blacksuan19 <i@blacksuan19.me>
2020-05-18 23:30:05 +0800

    assets: rename back.to.top.js to extra.js
    its not about back to top alone now
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

---
+++ assets/js/extra.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  // back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#scroll").fadeIn();
+    } else {
+      $("#scroll").fadeOut();
+    }
+  });
+  $("#scroll").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+
+  // logic for collpasable Headers
+  $(".col_head").click(function () {
+    $(this).nextAll("div").first().slideToggle("fast");
+  });
+});
+
